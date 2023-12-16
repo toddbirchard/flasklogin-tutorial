0c997a03cded0bf8336a115a529571f0d43fc67b
Todd Birchard <toddbirchard@gmail.com>
2023-12-16 15:19:56 -0500

    Password hashing update; minor style tweaks.

--- flask_login_tutorial/models.py
+++ flask_login_tutorial/models.py
@@ -20,7 +20,7 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         """Create hashed password."""
-        self.password = generate_password_hash(password, method="sha256")
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         """Check hashed password."""
